15201434a807e056947cb00a29c5315cac8e075e
Adrien Bustany <adrien@bustany.org>
2021-05-18 16:49:47 +0200

    Ensure icons are cached by the service worker

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,34 @@
+import crypto from 'crypto'
+import { readFileSync } from 'fs'
 import path from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueI18n from '@intlify/vite-plugin-vue-i18n'
 import { VitePWA } from 'vite-plugin-pwa'
+import { ManifestEntry } from 'workbox-build'
+
+function assetManifestEntry(assetPath: string): ManifestEntry {
+  const ASSETS_DIR = 'src/assets'
+
+  if (!assetPath.startsWith(ASSETS_DIR)) {
+    throw new Error(`Assets should be in ${ASSETS_DIR}`)
+  }
+
+  const fileContents = readFileSync(assetPath)
+  const fileStamp = crypto
+    .createHash('SHA256')
+    .update(fileContents)
+    .digest('hex')
+    .substring(0, 8)
+  const revision = crypto
+    .createHash('MD5')
+    .update(fileContents)
+    .digest('hex')
+  const parsedPath = path.parse(assetPath)
+  const url = `${parsedPath.dir.substr('src/'.length)}/${parsedPath.name}.${fileStamp}${parsedPath.ext}`
+
+  return {url, revision}
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -28,6 +54,11 @@ export default defineConfig({
       filename: 'sw.ts',
       base: '/',
       strategies: 'injectManifest',
+      injectManifest: {
+        additionalManifestEntries: [
+          assetManifestEntry('src/assets/icons.svg')
+        ],
+      },
       workbox: {
         navigateFallback: '/index.html',
       },
